f8d9130dede589bdf80af98917b266c7cb246697
SwagMal <bjarturhafthors@hotmail.com>
2016-02-21 22:04:31 +0000

    Resource errors fixed

--- client/src/resource/ChatResource.js
+++ client/src/resource/ChatResource.js
@@ -1,4 +1,8 @@
-angular.module("ChatApp").factory("ChatResource"; 
+"use strict";
+
+var socket = io.connect('http://localhost:8080');
+
+angular.module("ChatApp").factory("ChatResource");
 function ChatResource() {
 	return {
 		login: function login(nick) {
@@ -10,75 +14,65 @@ function ChatResource() {
 		},
 
 		getRoomList: function getRoomList() {
-			socket.emit("rooms"){
-				// TODO Capture Event
-			};
+			socket.emit("rooms");
 		},
 
 		createRoom: function createRoom() {
 			var roomInfo = {room: undefined};
 
-			socket.emit("joinroom", roomInfo, function(available, reason)){
+			socket.emit("joinroom", roomInfo, function(available, reason){
 				// TODO: Error handling, catch events
-			};
+			});
 		},
 
 		joinRoom: function joinRoom(roomID) {
 			var roomInfo ={room: roomID};
 
-			socket.emit("joinroom", roomInfo, function(available, reason)){
+			socket.emit("joinroom", roomInfo, function(available, reason){
 				// TODO: Error handling, catch events
-			};
+			});
 		},
 
 		leaveRoom: function leaveRoom(roomID) {
-			socket.emit("partroom", roomID){
-				// TODO: Catch event
-			};
+			socket.emit("partroom", roomID);
 		},
  
 		sendMessage: function sendMessage(message, roomID) {
 			var messageInfo = {roomName: roomID, msg: message};
 
-			socket.emit("sendmsg", messageInfo){
-				// TODO: Catch event
-			};
+			socket.emit("sendmsg", messageInfo);
 		},
 
 		sendPrivateMessage: function sendPrivateMessage(message, user) {
 			var messageInfo = {nick: user, msg: message};
 
-			socket.emit("sendmsg", messageInfo, function(available)){
+			socket.emit("sendmsg", messageInfo, function(available){
 				// TODO: Error handling, catch event
-			};
+			});
 		},
 
 		getUserList: function getUserList() {
-			socket.emit("users"){
-				// TODO: Catch event
-			};
+			socket.emit("users");
 		},
 
 		kickUser: function kickUser(nick, roomID) {
 			var userInfo = {user: nick, room: roomID};
 
-			socket.emit("kick", userInfo, function(available)){
+			socket.emit("kick", userInfo, function(available){
 				// TODO: Catch events, error handling
-			};
+			});
 		},
 
 		banUser: function banUser(nick, roomID) {
 			var userInfo = {user: nick, room: roomID};
 
-			socket.emit("ban", userInfo, function(available)){
+			socket.emit("ban", userInfo, function(available){
 				// TODO: Catch events, error handling
-			};
+			});
 		},
 
 		exit: function exit() {
-			socket.emit("disconnect"){
-				// TODO: Catch events
-			};
+			socket.emit("disconnect");
 		}
-	}
-});
+	};
+}
